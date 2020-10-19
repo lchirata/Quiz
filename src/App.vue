6ea2c7d3e10cea90ecb3ceafdea7aadc0e57187b
lchirata <larissa.c.hirata@gmail.com>
2020-10-18 21:47:21 -0300

    ultimo commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <Header />
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
+
     <b-container class="bv-example-row">
       <b-row>
-        <b-col sm="6" offset="3"> 
-           <!-- sm e set configura a posição do Grid -->
-          <QuestionBox 
+        <b-col sm="6" offset="3">
+          <QuestionBox
             v-if="questions.length"
             :currentQuestion="questions[index]"
             :next="next"
+            :increment="increment"
           />
         </b-col>
       </b-row>
@@ -17,43 +21,50 @@
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import QuestionBox from "./components/QuestionBox.vue";
-
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 export default {
-  name: "App",
+  name: 'app',
   components: {
     Header,
-    QuestionBox,
+    QuestionBox
   },
-  data(){
+  data() {
     return {
       questions: [],
-      index: 0
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
     }
   },
   methods: {
     next() {
       this.index++
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++
+      }
+      this.numTotal++
     }
   },
   mounted: function() {
     fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
-      method:'get'
+      method: 'get'
     })
-      .then((response)=>{
-        return response.json();
+      .then((response) => {
+        return response.json()
       })
-      .then((jsonData) =>{
+      .then((jsonData) => {
         this.questions = jsonData.results
       })
   }
-};
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
